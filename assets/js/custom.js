15b1b97e6f2f10b3343ba7f85b09d688809ad34a
Jimmy Tan <wjinz99@hotmail.com>
2023-12-01 01:54:48 +0800

    done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,35 @@
 $( document ).ready(function() {
-    new WOW().init();
+    new WOW({
+        mobile: true
+    }).init();
+
+    var owl = $(".owl-carousel").owlCarousel({
+        loop:true,
+        margin:30,
+        nav:false,
+        dots: false,
+        autoplay:true,
+        autoplayTimeout:2000,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
+
+    $('.next').click(function() {
+        owl.trigger('next.owl.carousel');
+    })
+
+    $('.prev').click(function() {
+        owl.trigger('prev.owl.carousel', [300]);
+    })
 
     $(".currentYear").text(new Date().getFullYear());
 });
